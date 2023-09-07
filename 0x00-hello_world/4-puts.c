4b8c2b64edb4ba72d7c5c56b45a3cd6424515bf2
YaraAminn <aminyara.94@gmail.com>
2023-09-07 15:36:45 -0700

    Puttss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,10 @@
 
 /** 
  * This is the first program using the c language
-  */
+ * main : it's where we write the program here we write in this program a function called "puts"
+ * return : return 0 its the end of the program
+
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
