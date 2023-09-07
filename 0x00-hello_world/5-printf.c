682f0e7d1a93d60ccfc8de984a284211de8f0a17
YaraAminn <aminyara.94@gmail.com>
2023-09-07 16:22:41 -0700

    Print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main => the start of the program
+ * Return => 0 means the success of the program
+ */
+int main(void){
+	
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);	
+}	
