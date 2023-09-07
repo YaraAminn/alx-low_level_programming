bc8acf35c142c0cf8ee6520792948e5d5d4c11b6
YaraAminn <aminyara.94@gmail.com>
2023-09-07 16:12:57 -0700

    Puuts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /** 
  * main : it's where we write the program here we write in this program a function called "puts"
- * Return :0  it's the success and end of the program
+ * return :0  it's the success and end of the program
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
