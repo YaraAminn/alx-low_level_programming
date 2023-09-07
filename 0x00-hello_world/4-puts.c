ed8a0a81abbaf0e335f33db37e043d1dffb4e0ee
YaraAminn <aminyara.94@gmail.com>
2023-09-07 15:27:03 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/** 
+ * This is the first program using the c language
+  */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (puts);
+  return 0;
+
+
+
+}
