122a9af0c4624820efee62731d0a7dcff81ebccb
YaraAminn <aminyara.94@gmail.com>
2023-09-08 16:24:18 -0700

    5th taskk

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description:A program that prints the alphabet in lowercase
+ *
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+
+}
+
