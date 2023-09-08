8cdcc96ef65a64d35d7c96e6ebeaeb983a9ce8b9
YaraAminn <aminyara.94@gmail.com>
2023-09-08 16:05:08 -0700

    5th task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description:A program that prints the alphabet in lowercase
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int l = 97;
+	while(l <= 122)
+	{
+		if (l == 101 || l == 113)
+		{
+			l++;
+			continue;
+		}
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+
+}
+
