5ef9fb74f57827830d8053815b60a632e04515c5
YaraAminn <aminyara.94@gmail.com>
2023-09-08 16:52:10 -0700

    7th task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description:A program that prints numbers from 0To9
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int numb = 0;
+
+	while (numb < = 9)
+	{
+		putchar(numb);
+		numb++;
+	}
+	putchar('\n');
+	return (0);
+
+}
+
