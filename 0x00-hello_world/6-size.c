9c9b204bd2724551f24af59207d9efe8c159a17b
YaraAminn <aminyara.94@gmail.com>
2023-09-09 22:48:48 +0000

    siizzze

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,6 @@ long long int l;
 	printf("Size of an int: %ld byte(s)\n", sizeof(a));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(b));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(l));
-	printf("Size of a float: %ld byte(s)", sizeof(z));
+	printf("Size of a float: %ld byte(s)\n", sizeof(z));
 return (0);
 }
