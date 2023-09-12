4f9a69b48faa1ff11ac7f0bd645577183104e334
YaraAminn <aminyara.94@gmail.com>
2023-09-12 18:42:38 +0000

    alphabet 10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- entry point
+ *
+ * Description:Program prints alphabet in lw_cse 10times
+ *
+ * Return: always 0(success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int x;
+
+	for (x=0; x < 10; x++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
+	}
+}
