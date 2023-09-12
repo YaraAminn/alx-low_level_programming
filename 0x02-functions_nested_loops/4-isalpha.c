9896abc26388387e08e1bb85a84c465726501302
YaraAminn <aminyara.94@gmail.com>
2023-09-12 20:17:58 +0000

    checks for any letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isalpha - Checks for alphabetic letter
+ *
+ * @c : The checked character
+ *
+ * Return : 1 for letter or 0 for anyother char
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+}
+
