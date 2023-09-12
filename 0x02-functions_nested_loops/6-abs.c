dfde93680fb602069677a7ffe00bfcdaf9cdd152
YaraAminn <aminyara.94@gmail.com>
2023-09-12 21:26:00 +0000

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _abs - Computes the abs value of a number
- *
- * @c : The computed number
- * Description : program that prints the asb val of num 
- * Return : Absolue value of a number or 0
- */
+*_abs - Computes the abs value of a number
+*
+* @c :The computed number
+* Description : program that prints the asb val of num
+* Return : Absolue value of a number or 0
+*/
 
 int _abs(int c)
 {
