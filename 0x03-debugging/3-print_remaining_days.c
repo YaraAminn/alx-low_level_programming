a9893f664714ec74546831c1107a7af5a9c492c8
YaraAminn <aminyara.94@gmail.com>
2023-09-14 20:26:35 +0000

    ya lattef

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,8 +23,9 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month == 2 && day == 60)
+if (month <= 2 && day <= 60)
 {
+day--;
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
