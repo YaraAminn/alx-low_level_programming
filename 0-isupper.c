1009febfb2db71d0be22ddcdf88cb1602ac2f18e
YaraAminn <aminyara.94@gmail.com>
2023-09-15 15:42:16 +0000

    upperletter

---
+++ 0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/** _isupper - Check if letter is uppercase
+ * 
+ * @c: The checked letter
+ *
+ * Return: 1 for uppcse lett, 0 for anything else
+ */
+
+_isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+
+
