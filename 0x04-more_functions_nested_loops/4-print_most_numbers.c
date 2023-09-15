5f666fd3960618ca8a106f0b32727b6521860cbf
YaraAminn <aminyara.94@gmail.com>
2023-09-15 19:37:20 +0000

    print nums

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
-/*
- * print_most_numbersprint_most_numbers - to print most numbers
+
+/**
+ * print_most_numbers - to print most numbers
  *
- * Return: success
+ * Return: void
  */
 
 void print_most_numbers(void)
