a748ed35db2af75bae90112cf3bd37fe6bac1ff9
YaraAminn <aminyara.94@gmail.com>
2023-09-15 21:02:20 +0000

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,9 @@ void print_line(int n)
 {
 	int a;
 
-	for (a = 0; a <= n; a++)
+	for (a = 0; a < n; a++)
 	{
-		if (n < 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
