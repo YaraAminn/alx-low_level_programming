4a96296f35a5f796b144d712e2df575909e1ea7e
YaraAminn <aminyara.94@gmail.com>
2023-10-05 05:12:48 +0300

    5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -47,7 +47,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++,x++)
+		for (j = 0; av[i][j] != '\0'; j++, x++)
 		{
 			s[x] = av[i][j];
 		}
