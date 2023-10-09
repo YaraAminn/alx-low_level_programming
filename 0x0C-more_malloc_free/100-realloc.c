4a506a7512d8c416272874fa6ccbab6c98b08d48
YaraAminn <aminyara.94@gmail.com>
2023-10-09 12:35:42 -0700

    4_alloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,7 +35,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			free(ptr);
 			return (NULL);
 		}
+
+		char *old_Ptr;
+
+
+		old_Ptr = ptr;
+
+		if (new_size > old_size)
+		{
+			for (i =0; i < new_size; i++)
+			{
+				new_ptr[i] = old_Ptr[i];
+			}
+		}
 	}
+
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 	{
