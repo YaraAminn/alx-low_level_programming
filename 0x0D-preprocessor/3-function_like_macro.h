f53a929f0fbacfd26c503e6ef18fbe2f61a0f214
YaraAminn <aminyara.94@gmail.com>
2023-10-10 11:47:41 -0700

    3-abs

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * File - 3-function_like_macro
+ *
+ * @x: input
+ *
+ * Description: a function that
+ * computes the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
