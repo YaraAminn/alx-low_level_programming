c364a5ec8abc7d332d70e7e101680d9f6022ebfa
YaraAminn <aminyara.94@gmail.com>
2023-10-10 10:45:31 -0700

    0macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * File: 0-object like macro header file
+ *
+ * Description: Make a macro header file
+ */
+#define SIZE 1024 
+
+#endif 
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This is a readme file made for the Preprocessing 0x0c task
