2759beabf8510f443e41b21564968eb58483c315
YaraAminn <aminyara.94@gmail.com>
2023-10-10 12:40:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a file made for the 0xcE task Structures
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - create information of dog
+ *
+ * @name: dog name string
+ * @age: age of dog in decimal
+ * @owner: dog owner char
+ *
+ * Description: data_structure in c
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
