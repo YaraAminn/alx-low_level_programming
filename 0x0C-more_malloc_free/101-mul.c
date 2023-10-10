7937be3a14bede16fdd001e4c1a4ef1ab6a98e2f
YaraAminn <aminyara.94@gmail.com>
2023-10-09 17:22:09 -0700

    5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,7 +4,6 @@
 /**
  * is_digit - check if a string contains
  * a non digit char
- *
  * @str: checked string
  *
  * Return: 0 if non digit, 1 if digit
@@ -22,12 +21,9 @@ int is_digit(char *str)
 	}
 	return (1);
 }
-
 /**
  * str_len - function to return string length
- *
  * @str: checked string
- *
  * Return: string length
  */
 int str_len(char *str)
@@ -40,7 +36,6 @@ int str_len(char *str)
 	}
 	return (i);
 }
-
 /**
  * Error - fix and solve the errors
  */
@@ -52,34 +47,25 @@ void Error(void)
 
 /**
  * main - function to multiply 2 pos nums
- *
  * @argc: no of arguments
  * @argv: array of arguments
- *
  * Return: 0 success
  */
-
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, hold, dig1, dig2, *res, a = 0;
 
-	s1 = argv[1];
-	s2 = argv[2];
+	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		Error();
-
-	len1 = str_len(s1);
-	len2 = str_len(s2);
+	len1 = str_len(s1), len2 = str_len(s2);
 	len = len1 + len2 + 1;
 	res = malloc(len * sizeof(int));
-
 	if (!res)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
-	{
 		res[i] = 0;
-	}
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		dig1 = s1[len1] - '0';
