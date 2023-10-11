341cdc341bac912843eb3b3bbd9e764903a58483
YaraAminn <aminyara.94@gmail.com>
2023-10-11 16:36:47 -0700

    operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,15 @@
+#include "3-calc.h"
+/**
+ * main - main function
+ *
+ * @argc: count input
+ * @argv: pointer argument
+ *
+ * Return: 0 success
+ *
+ */
+int main (int argc, char argc[])
+{
+
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - add two integers
+ *
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: sum of 2 numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts 2 integers
+ *
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: subtraction of 2 nums
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply 2 integers
+ *
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: product of 2 numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ *
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: division result of 2 nums
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - get the remainder of 2 nums
+ *
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: modulus of 2 nums
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
